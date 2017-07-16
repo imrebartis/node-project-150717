0abea0f8366be907c23a4a58bf8c798cc3b5cd6a
imrebartis <imbartis@gmail.com>
2017-07-16 22:12:11 +0300

    experimenting with mixing, started work on create store functionality

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,3 +2,12 @@ exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 };
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  console.log(req.body);
+  res.json(req.body); //this renders on http://localhost:7777/add the data submited in the form
+};
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,18 @@ const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/', storeController.homePage);
 
-storeSchema.pre('save', function(next) { //autogenerating the slug string before someone creates a store
-  if (!this.isModified('name')) {
-    next(); // skip it
-    return; // stop this function from running
-  }
-  this.slug = slug(this.name);
-  next();
-  // TODO make more resiliant so slugs are unique
-});
+router.get('/add', storeController.addStore);
+
+router.post('/add', storeController.createStore);
+
+// storeSchema.pre('save', function(next) { //autogenerating the slug string before someone creates a store
+//   if (!this.isModified('name')) {
+//     next(); // skip it
+//     return; // stop this function from running
+//   }
+//   this.slug = slug(this.name);
+//   next();
+//   // TODO make more resiliant so slugs are unique
+// });
 
 module.exports = router;
