68819e07de26f689a108c77061a4a9ea0a109c47
imrebartis <imbartis@gmail.com>
2017-07-16 15:09:41 +0300

    extending layout to hello.pug

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,12 @@ router.get('/', (req, res) => {
   // res.send('Hey! It works!');
   // res.json(bjim); //this will display on the page the bjim const as a json object
   // res.send(req.query.name); // http://localhost:7777/?name=bjim&age=100 will display on the page bjim (actually the name param can be anything, e.g. bjam will display bjam)
-  res.json(req.query); //this will display on the page any param in the url (e.g. http://localhost:7777/?name=bjam&age=120&cool=true) as a json object 
+  // res.json(req.query); //this will display on the page any param in the url (e.g. http://localhost:7777/?name=bjam&age=120&cool=true) as a json object 
+  res.render('hello', {
+    name: 'bjim',
+    dog: req.query.dog
+  }
+);
 });
 
 router.get('/reverse/:name', (req, res) => {
