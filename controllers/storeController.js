4e35badb47f8519922d030bb8bb347fd0854e1a1
imrebartis <imbartis@gmail.com>
2017-07-16 16:27:52 +0300

    Store model created, imported it to start.js, added storeSchema.pre in index.js

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,4 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'bjim';
-    res.cookie('name', 'bjim is cool', {maxAge: 9000000 })
-    next();
-}
-
 exports.homePage = (req, res) => {
-    res.render('index');
-}
+  console.log(req.name);
+  res.render('index');
+};
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,16 @@ const storeController = require('../controllers/storeController');
 
 
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+storeSchema.pre('save', function(next) { //autogenerating the slug string before someone creates a store
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resiliant so slugs are unique
+});
+
 module.exports = router;
