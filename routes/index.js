33c8be4c935319d12abf35cbbe8500fc21edd40d
imrebartis <imbartis@gmail.com>
2017-07-16 15:35:35 +0300

    storeController and index.pug created

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+
 
 // Do work here
-router.get('/', (req, res) => {
-  const bjim = { name: 'bjim', age: 100};
-  //req has all the info
-  //res has all the methods for sending data back
-  //req.query is for getting the query paramaters
-  //req.body is for posting paramaters
-  //req.params is for accessing things that are in the url
-
-
-  // res.send('Hey! It works!');
-  // res.json(bjim); //this will display on the page the bjim const as a json object
-  // res.send(req.query.name); // http://localhost:7777/?name=bjim&age=100 will display on the page bjim (actually the name param can be anything, e.g. bjam will display bjam)
-  // res.json(req.query); //this will display on the page any param in the url (e.g. http://localhost:7777/?name=bjam&age=120&cool=true) as a json object 
-  res.render('hello', {
-    name: 'bjim',
-    dog: req.query.dog,
-    title: 'I love food'
-  }
-);
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
